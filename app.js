5529d24805b094039046a51452c3bdb91d1eeaea
EmilyCr <crawford.emily@hotmail.com>
2015-03-26 13:30:49 -0400

    put

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var calculator = require('./routes/calculator');
 
 var app = express();
 
-var port = process.env.PORT || 8080;
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -36,22 +34,11 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-app.get('/', function(req, res) {
-    res.render('index');   
-}
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+
 
 // production error handler
 // no stacktraces leaked to user
